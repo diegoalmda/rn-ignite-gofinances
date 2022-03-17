0d5897dc01c02f058439db1e11c8e4f94fbd5e2f
Diego Almeida <diegoalmda@gmail.com>
2022-03-17 16:49:04 -0300

    Implementação do modal de categorias

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import {
 import theme from './src/global/styles/theme';
 // import { Dashboard } from './src/screens/Dashboard';
 import { Register } from './src/screens/Register';
+// import { CategorySelect } from './src/screens/CategorySelect';
 
 
 export default function App() {
@@ -27,6 +28,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      {/* <CategorySelect /> */}
       <Register />
       {/* <Dashboard /> */}
     </ThemeProvider>
