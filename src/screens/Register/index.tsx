288b9905eac1ac53d832c3172958b5362ec17387
Diego Almeida <diegoalmda@gmail.com>
2022-03-22 14:02:43 -0300

    Finalizando tela e gráfico de resumo de despesas

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -41,6 +41,7 @@ const schema = Yup.object().shape({
     .number()
     .typeError('Informe um valior numérico')
     .positive('O valor não pode ser negativo')
+    .required('O valor é obrigatório')
 });
 
 export function Register() {
